96d3bac38ccb350d7297e2042e99a40fc71bae92
PemudaPemudi <ajipinanggih06@gmail.com>
2022-04-12 10:12:19 +0700

    Finish migration Camp, Camp_benefit. Checkout

---
+++ testdb/database/factories/ProductFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    protected $model = Product::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'product_category_id'=> $this->faker->numberBetween(1, 3),
+            'name'=> $this->faker->word(),
+            'description'=> $this->faker->paragraph(), 
+            'price'=> $this->faker->randomNumber(), 
+            'image' => $this->faker->imageUrl(640,480, 'animals', true)
+            
+        ];
+    }
+}
